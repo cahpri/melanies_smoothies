e581f0ce276b47407fe72b1349b68602694c1615
Matthew McBride <41593366+cahpri@users.noreply.github.com>
2025-02-11 13:12:01 -0500

    Update streamlit_app.py
    Bug fix to nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ if ingredients_list:
 
     for fruit in ingredients_list:
         ingredients_string += fruit + ' '
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        st.subheader(fruit + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
